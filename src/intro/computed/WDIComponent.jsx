8d906474437e6402cf131daa32246ec9a7ddff5f
cra2ycoder <mohaa.r.6789@gmail.com>
2019-05-05 20:26:01 +0530

    decorators with inline implemented

--- src/intro/computed/WDIComponent.jsx
+++ src/intro/computed/WDIComponent.jsx
@@ -2,37 +2,35 @@ import React, { createRef } from 'react'
 import { observable, computed } from 'mobx'
 import { observer } from 'mobx-react'
 
-class BirdStore {
+@observer
+class BirdShop extends React.Component {
   @observable birdsList = []
 
   @computed get getBirds() {
     return this.birdsList.join(',')
   }
 
-  setBirds(bird) {
-    this.birdsList.push(bird)
-  }
-}
-
-@observer
-class BirdShop extends React.Component {
   constructor(props) {
     super(props)
 
     this.textInput = createRef()
   }
 
+  setBirds(newBird) {
+    this.birdsList.push(newBird)
+  }
+
   handleClick = e => {
     e.preventDefault()
     const birdName = this.textInput.current.value
-    birdStore.setBirds(birdName)
+    this.setBirds(birdName)
   }
 
   render() {
     return (
       <>
-        <div>Total Birds: {birdStore.birdsList.length}</div>
-        <div>Birds: {birdStore.getBirds}</div>
+        <div>Total Birds: {this.birdsList.length}</div>
+        <div>Birds: {this.getBirds}</div>
         <form>
           <input placeholder="enter your bird name!" ref={this.textInput} />
           <button onClick={this.handleClick}>ADD</button>
@@ -42,10 +40,5 @@ class BirdShop extends React.Component {
   }
 }
 
-/**
- * object creation
- */
-const birdStore = new BirdStore()
-
-export { BirdShop, birdStore }
+export { BirdShop }
 export default BirdShop
--- src/intro/computed/index.js
+++ src/intro/computed/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BirdShop } from './WDComponent'
+import { BirdShop as WDIBirdShop } from './WDIComponent'
 
 ReactDOM.render(
   <>
     <BirdShop />
+    <hr />
+    <WDIBirdShop />
   </>,
   document.getElementById('computed')
 )
